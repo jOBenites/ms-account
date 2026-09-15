369d1ed411b2dfb0c64845731d17adf91ea9193b
jOBenites <josueodarbenites@gmail.com>
2026-09-15 14:34:40 -0500

    refactor: remove unused import for anyLong in MovementServiceTest

--- src/test/java/com/bank/msaccount/service/MovementServiceTest.java
+++ src/test/java/com/bank/msaccount/service/MovementServiceTest.java
@@ -29,7 +29,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
